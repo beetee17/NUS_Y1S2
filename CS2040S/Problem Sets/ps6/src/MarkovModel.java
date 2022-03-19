16d26d182479803aebdf5e943371548bad66660e
beetee17 <brandon.thio@yahoo.com.sg>
2022-03-19 15:39:56 +0800

    first attempt

--- CS2040S/Problem Sets/ps6/src/MarkovModel.java
+++ CS2040S/Problem Sets/ps6/src/MarkovModel.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
 /**
@@ -16,6 +18,15 @@ public class MarkovModel {
 	// This is a special symbol to indicate no character
 	public static final char NOCHARACTER = (char) 0;
 
+	private int order;
+
+	/**
+	 * Use a symbol table that maps strings of length `order` to another symbol table;
+	 * the second symbol table maps ASCII characters to integers.
+	 * These integers represent the frequency that a given character follows the initial string.
+	 */
+	private HashMap<String, HashMap<Character, Integer>> table = new HashMap<>();
+
 	/**
 	 * Constructor for MarkovModel class.
 	 *
@@ -24,6 +35,7 @@ public class MarkovModel {
 	 */
 	public MarkovModel(int order, long seed) {
 		// Initialize your class here
+		this.order = order;
 
 		// Initialize the random number generator
 		generator.setSeed(seed);
@@ -31,33 +43,73 @@ public class MarkovModel {
 
 	/**
 	 * Builds the Markov Model based on the specified text string.
+	 * This method may be called multiple times on the same MarkovModel object.
+	 * Each repeated call is expected to build the MarkovModel again.
 	 */
 	public void initializeText(String text) {
-		// Build the Markov model here
+		this.table = new HashMap<>();
+		int len = text.length();
+		for (int i = 0; i < len - this.order; i++) {
+
+			String s = text.substring(i, i + this.order);
+			Character next = text.charAt(i + this.order);
+
+			HashMap<Character, Integer> subTable = this.table.get(s);
+
+			if (subTable == null)
+				subTable = new HashMap<>();
+			this.table.put(s, subTable);
+			subTable.put(next, subTable.getOrDefault(next, 0) + 1);
+		}
 	}
 
 	/**
 	 * Returns the number of times the specified kgram appeared in the text.
 	 */
 	public int getFrequency(String kgram) {
-		return 0;
+		HashMap<Character, Integer> subTable = this.table.get(kgram);
+		if (subTable == null) return 0;
+
+		int sum = 0;
+		for (Integer i : subTable.values()) {
+			sum += i;
+		}
+		return sum;
 	}
 
 	/**
 	 * Returns the number of times the character c appears immediately after the specified kgram.
 	 */
 	public int getFrequency(String kgram, char c) {
-		return 0;
+		HashMap<Character, Integer> subTable = this.table.get(kgram);
+		if (subTable == null) return 0;
+		else return subTable.getOrDefault(c, 0);
 	}
 
 	/**
 	 * Generates the next character from the Markov Model.
 	 * Return NOCHARACTER if the kgram is not in the table, or if there is no
 	 * valid character following the kgram.
+	 * To generate the random choice, you must use the pseudorandom number generator with the specified seed
 	 */
 	public char nextCharacter(String kgram) {
-		// See the problem set description for details
-		// on how to make the random selection.
-		return 'a';
+		HashMap<Character, Integer> subTable = this.table.get(kgram);
+
+		if (subTable == null) return NOCHARACTER;
+
+		Character[] characters = new Character[subTable.keySet().size()];
+
+		Arrays.sort(subTable.keySet().toArray(characters));
+
+		int kgramFrequency = getFrequency(kgram);
+		int randInt = this.generator.nextInt(kgramFrequency);
+		int sum = 0;
+
+		for (Character c : characters) {
+			sum += subTable.get(c);
+			if (sum > randInt) return c;
+		}
+
+		return NOCHARACTER;
 	}
 }
--- CS2040S/Problem Sets/ps6/src/TextGenerator.java
+++ CS2040S/Problem Sets/ps6/src/TextGenerator.java
@@ -9,22 +9,23 @@ public class TextGenerator {
     private static long seed;
 
     // Sets the random number generator seed
-    public static void setSeed(long s) {
+    public static void setSeed(long s)
+    {
         seed = s;
     }
 
     /**
-     * Reads in the file and builds the MarkovModel.
-     *
-     * @param order the order of the Markov Model
-     * @param fileName the name of the file to read
-     * @param model the Markov Model to build
-     * @return the first {@code order} characters of the file to be used as the seed text
+     * buildModel rads in the file and builds the MarkovModel
+     * @param order
+     * @param fileName
+     * @param model
+     * @return
      */
-    public static String buildModel(int order, String fileName, MarkovModel model) {
+    public static String buildModel(int order, String fileName, MarkovModel model)
+    {
         // Get ready to parse the file.
-        // StringBuffer is used instead of String as appending character to String is slow
-        StringBuilder text = new StringBuilder();
+        //StringBuffer is used instead of String as appending character to String is slow
+        StringBuffer text = new StringBuffer();
 
         // Loop through the text
         try {
@@ -34,18 +35,20 @@ public class TextGenerator {
             int fileSize = inputStream.available();
 
             // Read in the file, one character at a time.
-            for (int i = 0; i < fileSize; i++) {
+            for (int i=0; i<fileSize; i++)
+            {
                 // Read a character
-                char c = (char) inputStream.read();
+                char c = (char)inputStream.read();
                 text.append(c);
             }
 
-            // Make sure that length of input text is longer than requested Markov order
-            if (text.length() < order) {
+            //Make sure that length of input text is longer than requested Markov order
+            if(text.length() < order)
+            {
                 System.out.println("Text is shorter than specified Markov Order.");
                 return null;
             }
-        } catch (Exception e) {
+        } catch(Exception e){
             System.out.println("Problem reading file " + fileName + ".");
             return null;
         }
@@ -58,31 +61,32 @@ public class TextGenerator {
     /**
      * generateText outputs to stdout text of the specified length based on the specified seedText
      * using the given Markov Model.
-     *
-     * @param model the Markov Model to use
-     * @param seedText the initial kgram used to generate text
-     * @param order the order of the Markov Model
-     * @param length the length of the text to generate
+     * @param model
+     * @param seedText
+     * @param order
+     * @param length
      */
-    public static void generateText(MarkovModel model, String seedText, int order, int length) {
+    public static void generateText(MarkovModel model, String seedText, int order, int length){
         // Use the first order characters of the text as the starting string
         StringBuffer kgram = new StringBuffer();
         kgram.append(seedText);
 
         // Generate length characters
-        char charToAppend;
+        char charToAppend = MarkovModel.NOCHARACTER;
         int outLength = kgram.length();
-        while (outLength < length) {
+        while (outLength < length)
+        {
             // Get the next character from kgram sequence. The kgram sequence to use
             // is the sequence starting from ith position.
             charToAppend = model.nextCharacter(kgram.substring(outLength - order));
 
-            // If there is no next character, restart generation with initial kgram value which
-            // Starts from 0th position.
-            if (charToAppend != MarkovModel.NOCHARACTER) {
+            //if there is no next character, restart generation with initial kgram value which
+            //starts from 0th position.
+            if(charToAppend != MarkovModel.NOCHARACTER) {
                 kgram.append(charToAppend);
                 outLength++;
-            } else {
+            }
+            else{
                 // This prefix has never appeared in the text.
                 // Give up?
                 System.out.println(kgram);
@@ -90,19 +94,22 @@ public class TextGenerator {
             }
         }
 
-        // Output the generated characters, not including the initial seed.
+        //output the generated characters, not including the initial seed
         System.out.println(kgram);
     }
 
     /**
      * The main routine.  Takes 3 arguments:
-     * args[0]: the order of the Markov Model
-     * args[1]: the length of the text to generate
-     * args[2]: the filename for the input text
+     * 1. the order of the Markov Model
+     * 2. the length of the text to generate
+     * 3. the filename for the input text
+     * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // Check that we have three parameters
-        if (args.length != 3) {
+        if (args.length != 3)
+        {
             System.out.println("Number of input parameters are wrong.");
         }
 
